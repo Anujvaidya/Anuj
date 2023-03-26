62333e05a4195a65a5ef7eea17f2eb2c77907f41
Anujvaidya <anujvaidya5@gmail.com>
2023-03-26 18:33:28 +0530

    Spring Bean Creation

---
+++ Spring Bean Creation/src/com/model/Student.java
@@ -0,0 +1,16 @@
+package com.model;
+
+public class Student {
+
+	
+	public Student(){
+		
+		System.out.println("Student Class Constructor");
+		
+	}
+	
+	public void display()
+	{
+		System.out.println("Display method of Student class");
+	}
+}
